caf7d70cf6e04d66d0988965b6b0c44826d11180
anton <anton.baric99@gmail.com>
2023-09-01 14:01:28 +0200

    logo added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
 import Home from './pages/Home'
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
+import catsLogo from './assets/cats.svg'
 import ItemPage from './pages/ItemPage'
 
 function App() {
 
   return (
     <>
+      <div style={{ position: 'absolute', top: '0', left: '0', padding: '10px' }}>
+        <Link to={"/"}>
+          <img src={catsLogo} className="logo react" alt="React logo" />
+        </Link>
+      </div>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path="/item/:id" element={<ItemPage />} />
